4a6631d36987c509ce111cff27cedf32173f4e1e
Erik Gaasedelen <erikgaas@gmail.com>
2025-02-03 21:53:53 -0800

    add root main

---
+++ main.py
@@ -0,0 +1,32 @@
+from fasthtml.common import *
+from monsterui.all import *
+from app.ui import *
+
+
+hdrs = Theme.blue.headers()
+
+tailwind_config = """
+    <script>
+        tailwind.config = {
+            darkMode: ['class', '[class="uk-theme-dark"]'],  // Match uk-theme-dark
+            theme: {
+                extend: {}
+            }
+        }
+    </script>
+    """
+
+# Insert the tailwind config before the existing script
+hdrs.insert(-3, Safe(tailwind_config))
+
+app, rt = fast_app(hdrs=hdrs)
+
+@rt
+def index():
+    return Home()
+
+@rt("/blogposts")
+def blogs():
+    return ListBlogs()
+
+serve()
