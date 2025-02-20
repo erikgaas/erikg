fcc358cc7f56d815b153eccc93f666de7b0fa1a8
Erik Gaasedelen <erikgaas@gmail.com>
2025-02-19 18:11:08 -0800

    test

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ async def admin_login(request, session):
     form = await request.form()
     password = form.get('password')
     
-    print('hihih', os.getenv('ADMIN_PASSWORD'))
+    print('hihih', os.getenv('RAILWAY_PUBLIC_DOMAIN'))
     if password == os.getenv('ADMIN_PASSWORD'):
         session['admin_access'] = True
         return AdminPage(session)
